73deb447f0742bf5943d89a1a7f16a336df6357f
Samarth Meattle <samarth@meattle.com>
2023-09-30 15:55:07 +0530

    cleaned files

---
+++ cmdClass.py
@@ -0,0 +1,11 @@
+import os
+
+class Cmd:
+    def __init__(self):
+        self.account = input("Account Name: ")
+    def create(self, fileName):
+        file = open(f"{self.account}_{fileName}", 'x')
+    def exitProgram(self):
+        exit()
+    def kill(self, fileName):
+        os.remove(
