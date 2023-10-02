2ac3bcc894235264b67ca6418d280d6633610771
Samarth Meattle <samarth@meattle.com>
2023-10-02 18:05:37 +0530

    added exitMessage parameter

--- cmdClass.py
+++ cmdClass.py
@@ -5,8 +5,8 @@ class Cmd:
         self.account = input("Account Name: ")
     def create(self, fileName):
         file = open(f"{self.account}_{fileName}", 'x')
-    def exitProgram(self):
-        exit()
+    def exitProgram(self, exitMessage):
+        exit(exitMessage)
     def kill(self, fileName):
         os.remove(fileName)
     def open(self, fileName):
